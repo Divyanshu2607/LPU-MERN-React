bdc6e059e75ac69957ffc4a6b19822f00048dea8
Shantanu Shubham <shantanu0707@gmail.com>
2024-07-04 09:57:03 +0530

    [Add] Counter logic with props

--- src/App.js
+++ src/App.js
@@ -48,11 +48,38 @@ class App extends Component {
             }
             return count - 2;
           }}
+          startVal={0}
         />
         {/* Pass the text inside the button and the function to execute on click as a prop */}
-        <Counter />
+        <Counter
+          btn1Text="Multiply by 2"
+          btn2Text="Divide by 2"
+          btn1OnClick={(count) => {
+            return count * 2;
+          }}
+          btn2OnClick={(count) => {
+            if (count / 2 < 1) {
+              return 1;
+            }
+            return count / 2;
+          }}
+          startVal={1}
+        />
         {/* Multiply by 2 and Divide by 2 */}
-        <Counter />
+        <Counter
+          btn1Text="Square"
+          btn2Text="Square Root"
+          btn1OnClick={(count) => {
+            return count * count;
+          }}
+          btn2OnClick={(count) => {
+            if (Math.sqrt(count) < 1) {
+              return 1;
+            }
+            return Math.sqrt(count);
+          }}
+          startVal={2}
+        />
         {/* Power of 2 and square root */}
       </div>
     );
